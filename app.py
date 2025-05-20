045fc964409e9ea0ef2d65881697189aed642da5
OlgaManakova2021 <ManakovaOlga2012@yandex.ru>
2025-05-20 20:03:56 +0300

    Добавили главную страницу и страницу приветствия

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print('Hello!')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")   # 127.0.0.1:5000 + /  =  127.0.0.1:5000/
+def index():
+    return "Главная страница"
+
+@app.route("/example")
+def example():
+    return "Hello example!"
+
+
+if __name__ == "__main__":
+    app.run()
